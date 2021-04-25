b779369d67ff1340beb1b8df373a979c522dfbe3
3dsonneto <edson_neto1996@hotmail.com>
2021-04-25 18:58:17 -0300

    Tela base feita

--- lib/oddeven_view.dart
+++ lib/oddeven_view.dart
@@ -9,7 +9,45 @@ class _OddEvenScreenState extends State<OddEvenScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Text(
+              "Numero: XYZ",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w300,
+                  decoration: TextDecoration.none),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 30, bottom: 30),
+            child: Text(
+              "Par",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 35,
+                  fontWeight: FontWeight.w300,
+                  decoration: TextDecoration.none),
+            ),
+          ),
+          Container(
+            width: 120,
+            height: 40,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Restart",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
