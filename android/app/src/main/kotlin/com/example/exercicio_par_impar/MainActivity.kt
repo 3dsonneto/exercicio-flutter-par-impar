78dc202ea4f4eb0a1d6e40c7e6b74be596045589
3dsonneto <edson_neto1996@hotmail.com>
2021-04-25 18:39:31 -0300

    Separando o projeto em model, view e controller

---
+++ android/app/src/main/kotlin/com/example/exercicio_par_impar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exercicio_par_impar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/oddeven_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OddEvenScreen extends StatefulWidget {
+  @override
+  _OddEvenScreenState createState() => _OddEvenScreenState();
+}
+
+class _OddEvenScreenState extends State<OddEvenScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
